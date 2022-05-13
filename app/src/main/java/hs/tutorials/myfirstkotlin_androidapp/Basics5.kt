96200c15ce98895e040105dafc5a1198c2b6d212
hyesung6 <k_hyesung6@naver.com>
2022-05-13 23:15:38 +0900

    Kotlin - Android App

--- app/src/main/java/hs/tutorials/myfirstkotlin_androidapp/Basics5.kt
+++ app/src/main/java/hs/tutorials/myfirstkotlin_androidapp/Basics5.kt
@@ -1,17 +1,22 @@
 package hs.tutorials.myfirstkotlin_androidapp
 
 fun main() {
-    // null 값이 필요한 경우를 위해 nullable 변수를 지원
+    //    null 값이 필요한 경우를 위해 nullable 변수를 지원
     var name :String = "Denis"
-    // name = null -> Compilation Error
+    //    name = null -> Compilation Error
     var nullableName : String? = "Denis"
-    // nullableName = null    -> 가능
+    //    nullableName = null    -> 가능
 
     var len = name.length
     var len2 = nullableName?.length
     nullableName?.let {println(it.length)}
 
-    // ? : Elvis operator
+    //    ? : Elvis operator
     val names = nullableName ?: "Guest"
-    println("name is $names")
+    //    println("name is $names")
+
+    println(nullableName!!.toLowerCase())
+
+    //    val wifesAge: String? = user?.wife?.age ?: 0
+    //    user 클래스의 age 객체가 null 인지 아닌지 호출&확인 가능
 }
